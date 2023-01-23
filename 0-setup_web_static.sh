b6411e56be5580fb4edb898ed950b5706ac82147
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-01-23 19:25:12 +0200

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
+# Install NGINX
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+echo "<html><head></head><body>yasinalhadi.tech</body></html>" > /data/web_static/releases/test/index.html
+sed -i "s/# Only/\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/; \n\t\
+\tautoindex off;\n\t}\n \n\t# Only/" /etc/nginx/sites-available/default
+sudo service nginx restart
